ed9d26c2f0e64d66553196caf492374f8d1be11b
Alex Olson <olsonalexw@gmail.com>
2026-02-15 07:44:19 -0500

    fix: support for IPv6 addresses in LOCAL_NETWORKS routing rules

--- internal/firewall/routing.go
+++ internal/firewall/routing.go
@@ -17,6 +17,9 @@ func (fw *Firewall) SetupLocalNetworkRoutes(networks []string) error {
 			continue
 		}
 		args := []string{"rule", "add", "to", network, "table", "main", "priority", strconv.Itoa(priorityLocalNet)}
+		if strings.Contains(network, ":") {
+			args = append([]string{"-6"}, args...)
+		}
 		fw.log.Debug("exec: ip %s", strings.Join(args, " "))
 		if err := exec.Command("ip", args...).Run(); err != nil {
 			return err
@@ -25,13 +28,16 @@ func (fw *Firewall) SetupLocalNetworkRoutes(networks []string) error {
 	return nil
 }
 
-// CleanupLocalNetworkRoutes removes all routing rules at priority 100.
+// CleanupLocalNetworkRoutes removes all routing rules at priority 100 (IPv4 and IPv6).
 func (fw *Firewall) CleanupLocalNetworkRoutes() {
 	priority := strconv.Itoa(priorityLocalNet)
-	for {
-		if err := exec.Command("ip", "rule", "del", "priority", priority).Run(); err != nil {
-			break
+	for _, family := range [][]string{{}, {"-6"}} {
+		for {
+			args := append(family, "rule", "del", "priority", priority)
+			if err := exec.Command("ip", args...).Run(); err != nil {
+				break
+			}
+			fw.log.Debug("Removed routing rule at priority %d", priorityLocalNet)
 		}
-		fw.log.Debug("Removed routing rule at priority %d", priorityLocalNet)
 	}
 }
